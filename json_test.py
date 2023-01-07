26199bf6eb1090669af0eae946a12d41d2549335
Jay Lowe <48257497+ogjaylowe@users.noreply.github.com>
2023-01-06 18:41:45 -0800

    changed response obj to a text body

--- json_test.py
+++ json_test.py
@@ -2,4 +2,4 @@ import requests
 res = requests.post('http://127.0.0.1:5000/start_container/1/1/1', json={"FPS_CAP":"30", "DOCKER_IMAGE":"roboflow/inference-server:jetson-trt-udp"})
 
 if res.ok:
-    print(res.json())
+    print(res.text)
